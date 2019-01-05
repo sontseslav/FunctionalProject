af8a37112227b334608560330c7c828eb823bb5b
sontseslav <s.khvalinsky@gmail.com>
2019-01-05 23:14:12 +0200

    Minor changes

--- functional-playground/play.js
+++ functional-playground/play.js
@@ -4,7 +4,7 @@ const array = [1, 2, 3];
 forEach(array, (data) => console.log(data));
 forEach(array, (data) => console.log(15 * data));
 
-forEachObject({a: 1, b: 2}, (k,v)=> {console.log(`${k}: ${v}`);});
+forEachObject({ a: 1, b: 2 }, (k,v)=> {console.log(`${k}: ${v}`);});
 
 forEach([0,1,2,3,4,5,6,7,8,9], (number) => {
   unless(number % 2, () => {console.log(`${number} is even`);});
