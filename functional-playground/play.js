7b40de653875f69f738f6b8c7683d872cf67bf2d
sontseslav <s.khvalinsky@gmail.com>
2019-01-05 22:54:28 +0200

    Learning higher order function (chapter 03)

--- functional-playground/play.js
+++ functional-playground/play.js
@@ -1,4 +1,11 @@
-import forEach from '../lib/es8-functional.js';
+import {forEach, forEachObject, unless} from '../lib/es8-functional.js';
 
 const array = [1, 2, 3];
+forEach(array, (data) => console.log(data));
 forEach(array, (data) => console.log(15 * data));
+
+forEachObject({a: 1, b: 2}, (k,v)=> {console.log(`${k}: ${v}`);});
+
+forEach([0,1,2,3,4,5,6,7,8,9], (number) => {
+  unless(number % 2, () => {console.log(`${number} is even`);});
+});
