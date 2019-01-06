93c339575fb76749070ce46a702638fd51d1756a
sontseslav <s.khvalinsky@gmail.com>
2019-01-06 14:36:44 +0200

    Hiph order function: basics

--- functional-playground/play.js
+++ functional-playground/play.js
@@ -1,4 +1,4 @@
-import {forEach, forEachObject, unless} from '../lib/es8-functional.js';
+import {forEach, forEachObject, unless, times, every, some, sortBy} from '../lib/es8-functional.js';
 
 const array = [1, 2, 3];
 forEach(array, (data) => console.log(data));
@@ -9,3 +9,31 @@ forEachObject({ a: 1, b: 2 }, (k,v)=> {console.log(`${k}: ${v}`);});
 forEach([0,1,2,3,4,5,6,7,8,9], (number) => {
   unless(number % 2, () => {console.log(`${number} is even`);});
 });
+
+times(100, (number) => {
+  unless(number % 2, () => {console.log(`${number} is even`);});
+});
+
+console.log(
+  every([1,2,5,NaN,44], isNaN)
+);
+console.log(
+  every([NaN,NaN,NaN], isNaN)
+);
+console.log(
+  some([1,2,5,NaN,44], isNaN)
+);
+
+const people = [
+  {firstName: 'aaa', lastName: 'ccc'},
+  {firstName: 'ccc', lastName: 'bbb'},
+  {firstName: 'bbb', lastName: 'aaa'},
+];
+const peopleOrig = people.slice(0, people.length); //copy
+people.sort((a,b) => {
+  return a.firstName > b.firstName ? 1 : a.firstName < b.firstName ? -1 : 0;
+});
+console.log(people);
+console.log(peopleOrig);
+people.sort(sortBy('lastName'));
+console.log(people);
